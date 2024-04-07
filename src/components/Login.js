0a3cf0c9e1b8c946cc2b95320c9b498b7f1a5ee1
nakashimashin <nakashimashin0517@gmail.com>
2024-04-07 17:50:34 +0900

    ログイン機能実装

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,21 @@
+import { signInWithPopup } from 'firebase/auth';
 import React from 'react'
+import { auth, provider } from '../firebase';
+import { useNavigate } from 'react-router-dom';
 
-const Login = () => {
+const Login = ({ setIsAuth }) => {
+  const navigate = useNavigate();
+  const loginInWithGoogle = () => {
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true);
+      navigate("/");
+    });
+  };
   return (
     <div>
-      Login
+      <p>ログインして始める</p>
+      <button onClick={loginInWithGoogle}>Googleでログイン</button>
     </div>
   )
 }
