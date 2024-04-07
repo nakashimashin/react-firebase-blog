d4e20dc3b5d3c2fc24812cd94a5de1b4db882532
nakashimashin <nakashimashin0517@gmail.com>
2024-04-07 19:34:59 +0900

    ログでデータ取得

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Home.css'
+import { getDocs, collection } from 'firebase/firestore';
+import { db } from '../firebase';
 
 const Home = () => {
+    useEffect(() => {
+        const getPosts = async () => {
+            const data = await getDocs(collection(db, 'posts'));
+            console.log(data);
+            console.log(data.docs);
+            console.log(data.docs.map((doc) => ({ doc })));
+            console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id})));
+        };
+        getPosts();
+    }, []);
     return (
         <div className='homePage'>
             <div className='postContents'>
